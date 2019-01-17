b9827b93ad9dd0a318fda1244410b137a31b880f
HWCI <howardchoi7@gmail.com>
2019-01-17 17:46:32 +0800

    upload

---
+++ Assets/Plugins/EasyTouchBundle/EasyTouch/Examples/4.X/GameExemple/RunBall/Ball.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    // Update is called once per frame
+}
---
+++ Assets/Plugins/EasyTouchBundle/EasyTouch/Examples/4.X/UnityUI/UICompatibility/UICompatibility.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using HedgehogTeam.EasyTouch;
+
+public class UICompatibility : MonoBehaviour
+{
+    public void SetCompatibility(bool value)
+    {
+        EasyTouch.SetUICompatibily(value);
+    }
+}
