5cd25a9da8ade1bb650edbc877b68f9a8a058221
HWCI <howardchoi7@gmail.com>
2019-01-31 14:50:55 +0800

    idk

---
+++ Assets/Script/Interactable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    
+    [Serializable]
+    public class OnInteractHandler : UnityEvent
+    {
+    }
+    [SerializeField] public OnInteractHandler onInteract;
+
+    private GameObject target;
+
+    void Start()
+    {
+        target = this.GetComponent<GameObject>();
+    }
+    
+    
+}
